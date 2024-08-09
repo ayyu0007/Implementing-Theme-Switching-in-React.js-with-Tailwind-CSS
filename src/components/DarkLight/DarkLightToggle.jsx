8d8c6ebbeb1e968d9164e4b1220556101d91d2df
ayush <ayushrangani71530@gmail.com>
2024-08-09 15:10:42 +0530

    style: Changes in UI & Components

--- src/components/DarkLight/DarkLightToggle.jsx
+++ src/components/DarkLight/DarkLightToggle.jsx
@@ -1,96 +1,97 @@
 import React, { useState, useEffect } from 'react';
-import { FaCheck } from "react-icons/fa";
+import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
+import { cn } from "../../lib/utils";
+
+const themes = {
+  light: [
+    { name: "zinc", color: "bg-zinc-950" },
+    { name: "orange", color: "bg-orange-500" },
+    { name: "red", color: "bg-red-500" },
+    { name: "blue", color: "bg-blue-500" },
+    { name: "green", color: "bg-green-500" },
+  ],
+  dark: [
+    { name: "zinc", color: "bg-zinc-100" },
+    { name: "orange", color: "bg-orange-500" },
+    { name: "red", color: "bg-red-500" },
+    { name: "blue", color: "bg-blue-500" },
+    { name: "green", color: "bg-green-500" },
+  ],
+};
 
 const DarkLightToggle = () => {
-    const [selectedColor, setSelectedColor] = useState(null);
-    const [mode, setMode] = useState(() => {
-        if (typeof window !== "undefined") {
-            return localStorage.getItem("mode") || "light";
-        }
-        return "light"; // Default to "light" mode if window is undefined
-    });
+  const [mode, setMode] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("mode") || "light";
+    }
+    return "light"; // Default to "light" mode if window is undefined
+  });
+  const { theme, setTheme } = useTheme();
 
-    const colors = {
-        light: [
-            { name: "blue", color: "bg-blue-600", id: 1 },
-            { name: "red", color: "bg-red-600", id: 2 },
-            { name: "green", color: "bg-green-600", id: 3 },
-            { name: "zinc", color: "bg-zinc-900", id: 4 },
-            { name: "amber", color: "bg-amber-600", id: 5 },
-        ],
-        dark: [
-            { name: "blue", color: "bg-blue-600", id: 1 },
-            { name: "red", color: "bg-red-600", id: 2 },
-            { name: "green", color: "bg-green-600", id: 3 },
-            { name: "zinc", color: "bg-zinc-900", id: 4 },
-            { name: "amber", color: "bg-amber-600", id: 5 },
-        ],
-    };
+  useEffect(() => {
+    localStorage.setItem("mode", mode);
+  }, [mode]);
 
-    useEffect(() => {
-        const savedColor = localStorage.getItem('selectedColor');
-        if (savedColor) {
-            setSelectedColor(parseInt(savedColor));
-        }
+  const handleModeChange = (newMode) => {
+    const selectedTheme = theme?.split("-")[1];
+    const newTheme = `${newMode}-${selectedTheme}`;
+    setMode(newMode);
+    setTheme(newTheme);
+  };
 
-        const storedMode = localStorage.getItem("mode");
-        if (storedMode) {
-            setMode(storedMode);
-        }
-    }, []);
+  return (
+    <div className="col-span-1 bg-background border-accent border-2 text-accent rounded-lg p-6 mt-4 shadow-lg">
+      <h3 className="text-xl font-semibold mb-4 border-b border-accent pb-2">Update Account Settings</h3>
 
-    useEffect(() => {
-        localStorage.setItem("mode", mode);
-    }, [mode]);
-
-    const handleClick = (id) => {
-        setSelectedColor(id);
-        localStorage.setItem('selectedColor', id);
-    };
-
-    const handleModeChange = (newMode) => {
-        setMode(newMode);
-    };
-
-    return (
-        <div className="col-span-1 bg-background border-accent border-2 text-accent rounded-lg p-4 mt-4">
-            <h3 className="text-xl font-semibold mb-2">Update Account Setting</h3>
-            <div className="mb-4">
-                <label className="block text-accent mb-2">Dark Mode</label>
-                <div className="flex items-center space-x-4">
-                    <button
-                        className={`mt-4 bg-accent text-white px-4 py-2 rounded ${mode === "light" && "border-2 border-border"}`}
-                        onClick={() => handleModeChange("light")}
-                    >
-                        <Sun className="w-5 h-5 shrink-0" /> Light
-                    </button>
-                    <button
-                        className={`mt-4 bg-accent text-white px-4 py-2 rounded ${mode === "dark" && "border-2 border-border"}`}
-                        onClick={() => handleModeChange("dark")}
-                    >
-                        <Moon className="w-5 h-5 shrink-0" /> Dark
-                    </button>
-                </div>
-            </div>
-            <div className="mb-4">
-                <span className="block text-accent mb-2">Color Theme</span>
-                <div className="flex space-x-2">
-                    {colors[mode]?.map((colorObj) => (
-                        <div
-                            key={colorObj.id}
-                            className={`w-8 h-8 cursor-pointer rounded-full ${colorObj.color} flex items-center justify-center`}
-                            onClick={() => handleClick(colorObj.id)}
-                        >
-                            {selectedColor === colorObj.id && (
-                                <FaCheck className="w-4 h-4 text-white" />
-                            )}
-                        </div>
-                    ))}
-                </div>
-            </div>
+      <div className="mb-6">
+        <h4 className="text-lg font-medium mb-2">Customize Theme</h4>
+        <div className="flex flex-col gap-3">
+          <button
+            className={cn(
+              "flex items-center gap-x-2 py-2 px-4 rounded-md text-sm border border-accent hover:bg-background transition",
+              mode === "light" && "border-2 border-border"
+            )}
+            onClick={() => handleModeChange("light")}
+          >
+            <Sun className="w-5 h-5" />
+            Light
+          </button>
+          <button
+            className={cn(
+              "flex items-center gap-x-2 py-2 px-4 rounded-md text-sm border border-accent hover:bg-background transition",
+              mode === "dark" && "border-2 border-border"
+            )}
+            onClick={() => handleModeChange("dark")}
+          >
+            <Moon className="w-5 h-5" />
+            Dark
+          </button>
         </div>
-    );
+      </div>
+
+      <div>
+        <h4 className="text-lg font-medium mb-2">Colors</h4>
+        <div className="grid grid-cols-2 gap-4">
+          {themes[mode]?.map((themeOption) => (
+            <button
+              key={themeOption.name}
+              onClick={() => setTheme(mode + "-" + themeOption.name)}
+              className={cn(
+                "flex items-center gap-x-2 border border-accent py-2 px-4 rounded-md text-sm text-foreground hover:bg-background transition ease-in",
+                theme === mode + "-" + themeOption.name && "border-2 border-border"
+              )}
+            >
+              <div
+                className={`w-6 h-6 rounded-full border shrink-0 ${themeOption.color}`}
+              />
+              {themeOption.name}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DarkLightToggle;
